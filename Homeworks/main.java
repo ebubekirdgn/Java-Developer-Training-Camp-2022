a5a602774d87474335c36f358bbc19f4bba827af
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2022-09-15 17:46:17 +0300

    clean code lines

--- Homeworks/main.java
+++ Homeworks/main.java
@@ -1,18 +1,9 @@
-/******************************************************************************
-
-                            Online Java Compiler.
-                Code, Compile, Run and Debug java program online.
-Write your code in this editor and press "Run" button to execute it.
-
-*******************************************************************************/
-
 public class Main
 {
 	public static void main(String[] args) {
 		
 		//Öne Çıkanlar
-		
-		
+	
 		String [] images= {"1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg","7.jpg","8.jpg","9.jpg","10.jpg",};
 		
 		String leftArrowDirection="";
@@ -47,8 +38,5 @@ public class Main
 	    for(int i=0; i < categories.length;i++){
 	           System.out.println(categories[i]);
         }
-        	
-
 	}
-	
 }
