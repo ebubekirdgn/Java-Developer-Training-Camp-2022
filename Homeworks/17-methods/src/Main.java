d3616062f90e09a73e1df7669aec7194851fc0a8
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2022-09-18 21:34:19 +0300

    methods

---
+++ Homeworks/17-methods/src/Main.java
@@ -0,0 +1,28 @@
+﻿public class Main {
+    public static void main(String[] args) {
+        sayiBulmaca();
+    }
+    
+    public static void sayiBulmaca(){
+        int[] sayilar = new int[]{1, 2, 5, 7, 9, 0};
+        int aranacak = 3;
+        boolean listedeVarMi = false;
+
+        for (int sayi : sayilar) {
+            if (sayi == aranacak) {
+                listedeVarMi = true;
+                break;
+            }
+        }
+
+        if (listedeVarMi) {
+           mesajVer(aranacak + " sayısı listede var.");
+        } else {
+           mesajVer(aranacak + " sayısı listede bulunamadı.");
+        }
+    }
+    
+      public static void mesajVer(String mesaj){
+          System.out.println(mesaj);
+      }
+}
