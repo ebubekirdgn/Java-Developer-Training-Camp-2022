8bd63d72cc8faeabef888bbe9fa28ef47ab7f556
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2022-09-21 20:25:30 +0300

    classesWithAttribute

---
+++ Homeworks/19-reCapDemo_Classes/reCapDemo_Classes/src/DortIslem.java
@@ -0,0 +1,11 @@
+public class DortIslem{
+    public int Topla(int s1,int s2){
+        return s1+s2;
+    }
+    public int Cikar(int s1,int s2){
+        return s1-s2;
+    }
+    public int Carp(int s1,int s2){
+        return s1*s2;
+    }
+}
