02f9381b77723f1f18ec69ab26063cd8e4fc35e7
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2022-10-05 22:52:05 +0300

    update

--- Lessons/05-10-2022/oopWithNLayeredApp/src/oopWithNLayeredApp/Main.java
+++ Lessons/05-10-2022/oopWithNLayeredApp/src/oopWithNLayeredApp/Main.java
@@ -1,6 +1,8 @@
 package oopWithNLayeredApp;
 
 import oopWithNLayeredApp.business.ProducManager;
+import oopWithNLayeredApp.dataAccess.HibernateProductDao;
+import oopWithNLayeredApp.dataAccess.JdbcProductDao;
 import oopWithNLayeredApp.entities.Product;
 
 public class Main {
@@ -8,7 +10,7 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		Product product1 = new Product(1,"IPhone Xr",10);
-		ProducManager producManager = new ProducManager();
+		ProducManager producManager = new ProducManager(new HibernateProductDao());
 		producManager.add(product1);
 	}
 
--- Lessons/05-10-2022/oopWithNLayeredApp/src/oopWithNLayeredApp/business/ProducManager.java
+++ Lessons/05-10-2022/oopWithNLayeredApp/src/oopWithNLayeredApp/business/ProducManager.java
@@ -2,20 +2,31 @@ package oopWithNLayeredApp.business;
 
 import oopWithNLayeredApp.dataAccess.HibernateProductDao;
 import oopWithNLayeredApp.dataAccess.JdbcProductDao;
+import oopWithNLayeredApp.dataAccess.ProductDao;
 import oopWithNLayeredApp.entities.Product;
 
 public class ProducManager {
+	private ProductDao productDao;
+	
+	public ProducManager(ProductDao productDao) {
+		 this.productDao = productDao;
+	}
+	
 	public void add(Product product) throws Exception {
 		// iş kuralları buraya yazılacak
 		if(product.getUnitPrice() < 10) {
 			throw new Exception("Ürün fiyatı 10 dan küçük olamaz.");
 		}
-		JdbcProductDao productDao = new JdbcProductDao();
-		productDao.add(product);
+		/*JdbcProductDao productDao = new JdbcProductDao();
+		productDao.add(product);*/
 
 		
-		HibernateProductDao hibernateProductDao = new HibernateProductDao();
-		hibernateProductDao.add(product);
+		/*HibernateProductDao hibernateProductDao = new HibernateProductDao();
+		hibernateProductDao.add(product);*/
+		
+		
+		//bir katman baska bir katmanın classını kullanıyorken sadece interfaceinden erisim kurmalıdır.
+		//ProductDao productDao = new JdbcProductDao();
+		productDao.add(product);
 	}
-	
-}
+ }
