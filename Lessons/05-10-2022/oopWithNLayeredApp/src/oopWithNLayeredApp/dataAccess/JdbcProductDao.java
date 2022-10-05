f2afc2e15acdd4a138e85d695113003b8a145fc4
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2022-10-05 22:04:42 +0300

    entities update

---
+++ Lessons/05-10-2022/oopWithNLayeredApp/src/oopWithNLayeredApp/dataAccess/JdbcProductDao.java
@@ -0,0 +1,12 @@
+package oopWithNLayeredApp.dataAccess;
+
+import oopWithNLayeredApp.entities.Product;
+
+public class JdbcProductDao {
+	public void add(Product product) {
+		System.out.println("JDBC ile veritabanına "+ product +" eklendi.");
+	}
+	public void update() {
+		
+	}
+}
