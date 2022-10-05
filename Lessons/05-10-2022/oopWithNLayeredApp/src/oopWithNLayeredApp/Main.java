0a2e00249c7a3de26a4503f26905d5e26480c702
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2022-10-05 23:38:26 +0300

    finished

--- Lessons/05-10-2022/oopWithNLayeredApp/src/oopWithNLayeredApp/Main.java
+++ Lessons/05-10-2022/oopWithNLayeredApp/src/oopWithNLayeredApp/Main.java
@@ -1,6 +1,11 @@
 package oopWithNLayeredApp;
 
+import java.util.List;
+import java.util.logging.Logger;
+
 import oopWithNLayeredApp.business.ProducManager;
+import oopWithNLayeredApp.core.logging.FileLogger;
+import oopWithNLayeredApp.core.logging.MailLogger;
 import oopWithNLayeredApp.dataAccess.HibernateProductDao;
 import oopWithNLayeredApp.dataAccess.JdbcProductDao;
 import oopWithNLayeredApp.entities.Product;
@@ -10,8 +15,11 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		Product product1 = new Product(1,"IPhone Xr",10);
-		ProducManager producManager = new ProducManager(new HibernateProductDao());
-		producManager.add(product1);
+		
+		 Logger[] loggers =  {};
+		
+		// ProducManager producManager = new ProducManager(new HibernateProductDao(), loggers);
+		 //producManager.add(product1);
 	}
 
 }
--- Lessons/05-10-2022/oopWithNLayeredApp/src/oopWithNLayeredApp/business/ProducManager.java
+++ Lessons/05-10-2022/oopWithNLayeredApp/src/oopWithNLayeredApp/business/ProducManager.java
@@ -1,5 +1,8 @@
 package oopWithNLayeredApp.business;
 
+import java.util.List;
+
+import oopWithNLayeredApp.core.logging.Logger;
 import oopWithNLayeredApp.dataAccess.HibernateProductDao;
 import oopWithNLayeredApp.dataAccess.JdbcProductDao;
 import oopWithNLayeredApp.dataAccess.ProductDao;
@@ -7,9 +10,12 @@ import oopWithNLayeredApp.entities.Product;
 
 public class ProducManager {
 	private ProductDao productDao;
+	private Logger[] loggers;
 	
-	public ProducManager(ProductDao productDao) {
+	public ProducManager(ProductDao productDao,Logger[] loggers) {
 		 this.productDao = productDao;
+		 this.loggers = loggers;
+
 	}
 	
 	public void add(Product product) throws Exception {
@@ -28,5 +34,9 @@ public class ProducManager {
 		//bir katman baska bir katmanın classını kullanıyorken sadece interfaceinden erisim kurmalıdır.
 		//ProductDao productDao = new JdbcProductDao();
 		productDao.add(product);
+		
+		for (Logger logger : loggers) {
+			logger.log(product.getName());
+		}
 	}
  }
